5c5f156017b72d591dd67b3f5658d6f415c3cb5f
Mileshaase <mileshaase.imperium@gmail.com>
2022-09-08 16:41:33 -0400

    comments

--- Blackjack.py
+++ Blackjack.py
@@ -12,19 +12,38 @@ playerWins = 0
 ties = 0
 gameExited = False
 
-def hold(): #performs the hold function for the dealer
-    if(gameExited == False):
-        global numOfGames
-        global dealerHand
-        dealerHand = rng.next_int(11) + 16
-        print(f"\nDealer's hand: {dealerHand}\nYour hand is: {hand}\n ")
-        checkWin("Hold")
-        numOfGames = numOfGames + 1
 
-def statistics(): #prints the statsistics for the user
+def startGame(): #runs the begining of the game
     if(gameExited == False):
-        percentageOfPlayerWins = round((100*(playerWins / (numOfGames-1))), 1)
-        print(f"Number of Player wins: {playerWins}\nNumber of Dealer wins: {dealerWins}\nNumber of tie games: {ties}\nTotal # of games played is: {numOfGames - 1}\nPercentage of Player wins: {percentageOfPlayerWins}%\n ")
+        global hand
+        global dealerHand
+        hand = 0
+        dealerHand = 0
+        print(f"START GAME #{numOfGames}")
+        dealCard() #starts the next process after the game begins
+
+
+def printMenu(): #prints out the menu options to the user
+    if(gameExited == False):
+        global menuChoice
+        print("1. Get another card\n2. Hold hand\n3. Print statistics\n4. Exit\n ")
+        menuChoice = input("Choose an option: ")
+        checkMenuSelection()
+        if(gameExited == True):
+            exit()
+
+def checkMenuSelection(): #checks what choice on the menu the user chose
+    global gameExited 
+    if(menuChoice == "1"): #checks what method to run next based on what the user chooses
+        dealCard()
+    elif(menuChoice == "2"):
+        hold()
+    elif(menuChoice == "3"):
+        statistics()
+    elif(menuChoice == "4"):
+        gameExited = True
+    else:
+        print(f"Invalid input!\nPlease enter an integer value between 1 and 4.\n ")
         printMenu()
 
 def dealCard(): #deals a card from the dealer
@@ -33,8 +52,8 @@ def dealCard(): #deals a card from the dealer
         global cardName
         global hand
 
-        cardValue = rng.next_int(13) + 1
-        if(cardValue == 1):
+        cardValue = rng.next_int(13) + 1 #calls a random value for the card
+        if(cardValue == 1): #determines the name or value of the card through ifs
             cardName = "ACE"
         elif(cardValue >= 2 and cardValue <= 10):
             cardName = str(cardValue)
@@ -48,39 +67,16 @@ def dealCard(): #deals a card from the dealer
             cardName = "KING"
             cardValue = 10
         hand = cardValue + hand
-        checkWin("Deal")
+        checkWin("Deal") #checks whether or not a player has won
 
-def startGame(): #runs the begining of the game
+def hold(): #performs the hold function for the dealer
     if(gameExited == False):
-        global hand
+        global numOfGames
         global dealerHand
-        hand = 0
-        dealerHand = 0
-        print(f"START GAME #{numOfGames}")
-        dealCard()
-
-def checkMenuSelection(): #checks what choice on the menu the user chose
-    global gameExited 
-    if(menuChoice == "1"):
-        dealCard()
-    elif(menuChoice == "2"):
-        hold()
-    elif(menuChoice == "3"):
-        statistics()
-    elif(menuChoice == "4"):
-        gameExited = True
-    else:
-        print(f"Invalid input!\nPlease enter an integer value between 1 and 4.\n ")
-        printMenu()
-
-def printMenu(): #prints out the menu options to the user
-    if(gameExited == False):
-        global menuChoice
-        print("1. Get another card\n2. Hold hand\n3. Print statistics\n4. Exit\n ")
-        menuChoice = input("Choose an option: ")
-        checkMenuSelection()
-        if(gameExited == True):
-            exit()
+        dealerHand = rng.next_int(11) + 16
+        print(f"\nDealer's hand: {dealerHand}\nYour hand is: {hand}\n ")
+        checkWin("Hold")
+        numOfGames = numOfGames + 1
 
 def checkWin(type): #checks whether or not the dealer or user won
     if(gameExited == False):
@@ -133,6 +129,11 @@ def checkWin(type): #checks whether or not the dealer or user won
             print(f"\nYour card is a {cardName}!\nYour hand is: {hand}\n ")
             printMenu()
 
+def statistics(): #prints the statsistics for the user
+    if(gameExited == False):
+        percentageOfPlayerWins = round((100*(playerWins / (numOfGames-1))), 1)
+        print(f"Number of Player wins: {playerWins}\nNumber of Dealer wins: {dealerWins}\nNumber of tie games: {ties}\nTotal # of games played is: {numOfGames - 1}\nPercentage of Player wins: {percentageOfPlayerWins}%\n ")
+        printMenu()
 
 def main(): #Starts the main function with a loop to easily repeat and exit the game
     while gameExited == False:
